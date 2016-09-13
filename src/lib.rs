f42372ad09cc8461570456eb95deee158e764abb
Nick Babcock <nbabcock19@hotmail.com>
2016-09-13 16:20:18 -0400

    Polish doctstring in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 //! # boxcars (also written boxca-rs)
 //!
 //! boxcars is an example of a [Rocket League](http://www.rocketleaguegame.com/) replay parser
-//! written in rust using [nom](https://github.com/Geal/nom) for parsing and
+//! written in Rust using [nom](https://github.com/Geal/nom) for parsing and
 //! [serde](https://github.com/serde-rs/serde) for serialization. Emphasis on example, as this
 //! library in no way competes with the other feature complete parsers such as
 //! [Octane](https://github.com/tfausak/octane) and
-//! [RocketLeagueReplayParser](https://github.com/jjbott/RocketLeagueReplayParser). Rather let the
-//! project be a good example of Rust code using nom, and serde as extensive examples are hard to
-//! come by. While lacking feature completeness and user friendly error message -- among other
-//! issues, tests and documentation strive to be thorough.
+//! [RocketLeagueReplayParser](https://github.com/jjbott/RocketLeagueReplayParser). Rather, let
+//! boxcars be a good example of Rust code using nom, and serde as extensive examples are hard to
+//! come by. While lacking feature completeness and user friendly error message -- [among other
+//! issues](https://github.com/nickbabcock/boxcars/issues), tests and documentation strive to be
+//! thorough.
 //!
 //! ```
 //! # let filename = "assets/rumble.replay";
