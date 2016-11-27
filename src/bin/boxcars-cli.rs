0d9d2e80c1bb310b68e164beda486f12d04072a5
Nick Babcock <nbabcock19@hotmail.com>
2016-11-26 19:20:38 -0500

    Add afl-rs fuzzing

--- src/bin/boxcars-cli.rs
+++ src/bin/boxcars-cli.rs
@@ -3,6 +3,10 @@
 //! The command line version of the library. Given a file path argument, it will ingest all the
 //! data into memory, and attempt parsing. If the parsing is successful, JSON is outputted to
 //! stdout, else a non-helpful error message is printed. Sorry!
+#![cfg_attr(feature = "afl-feat", feature(plugin))]
+#![cfg_attr(feature = "afl-feat", plugin(afl_plugin))]
+#![cfg(feature = "afl-feat")]
+extern crate afl;
 extern crate boxcars;
 extern crate nom;
 extern crate serde_json;
@@ -12,6 +16,7 @@ use std::fs::File;
 use nom::IResult;
 use std::io::Read;
 
+#[cfg(not(feature = "afl-feat"))]
 fn main() {
     let mut args: Vec<_> = env::args().collect();
     let filename = args.remove(1);
@@ -28,4 +33,11 @@ fn main() {
             println!("Oh no we failed to parse");
         }
     }
+}
+
+#[cfg(feature = "afl-feat")]
+fn main() {
+    afl::handle_bytes(|s| {
+        let _ = boxcars::parse(&s, true);
+    });
 }
