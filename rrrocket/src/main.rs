c20931eb90045e8053ff81b8116b6361de760841
Nick Babcock <nbabcock19@hotmail.com>
2017-10-23 17:46:45 -0400

    Add an actual commandline parser

--- rrrocket/src/main.rs
+++ rrrocket/src/main.rs
@@ -3,26 +3,35 @@
 //! The command line version of the library. Given a file path argument, it will ingest all the
 //! data into memory, and attempt parsing. If the parsing is successful, JSON is outputted to
 //! stdout, else a non-helpful error message is printed. Sorry!
+#[macro_use]
+extern crate structopt_derive;
+extern crate structopt;
 extern crate boxcars;
 extern crate serde_json;
 
-use std::env;
+use structopt::StructOpt;
 use std::fs::File;
 use std::io::{self, Read};
 
-fn main() {
-    let mut args: Vec<_> = env::args().collect();
-    let filename = args.remove(1);
-    let mut f = File::open(filename).unwrap();
+#[derive(StructOpt, Debug, Clone, PartialEq)]
+#[structopt(name = "rrrocket", about = "Parses a Rocket League replay file and outputs JSON")]
+struct Opt {
+    #[structopt(short = "c", long = "crc-check", help = "validate replay is not corrupt", default_value = "true")]
+    crc: bool,
+
+    #[structopt(help = "Rocket League replay file")]
+    input: String,
+}
+
+fn parse_file(input: &String, crc: bool) -> boxcars::Replay {
+    let mut f = File::open(input).unwrap();
     let mut buffer = vec![];
     f.read_to_end(&mut buffer).unwrap();
-    let b = boxcars::parse(&buffer, true);
-    match b {
-        Ok(val) => {
-            serde_json::to_writer(&mut io::stdout(), &val).unwrap();
-        }
-        _ => {
-            println!("Oh no we failed to parse");
-        }
-    }
+	boxcars::parse(&buffer, crc).unwrap()
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let data = parse_file(&opt.input, opt.crc);
+	serde_json::to_writer(&mut io::stdout(), &data).unwrap();
 }
