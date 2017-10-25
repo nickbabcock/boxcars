fe355f85fdba8de7281f3dd923751f86d7cebdc3
Nick Babcock <nbabcock19@hotmail.com>
2017-10-25 07:00:04 -0400

    Move to cargo workspaces to fix ci build

--- ci/script.sh
+++ ci/script.sh
@@ -14,8 +14,8 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    (cd rrrocket && cross run --target $TARGET -- --help)
-    (cd rrrocket && cross run --target $TARGET --release -- --help)
+    (cross run -p rrrocket --target $TARGET -- --help)
+    (cross run -p rrrocket --target $TARGET --release -- --help)
 }
 
 # we don't run the "test phase" when doing deploys
