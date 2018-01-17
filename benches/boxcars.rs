bdfdb2db1f5e56bcf75e14f1a6d4bc9c5406c7b2
Nick Babcock <nbabcock19@hotmail.com>
2018-01-17 08:01:00 -0500

    Move to criterion.rs for benchmarks

---
+++ benches/boxcars.rs
@@ -0,0 +1,76 @@
+#[macro_use]
+extern crate criterion;
+extern crate boxcars;
+extern crate serde_json;
+
+use criterion::{black_box, Criterion};
+use boxcars::*;
+use boxcars::crc::calc_crc;
+use std::io;
+
+fn bench_crc(c: &mut Criterion) {
+    c.bench_function("bench_crc", |b| {
+        let data = include_bytes!("../assets/rumble.replay");
+        b.iter(|| {
+            black_box(calc_crc(&data[..]));
+        })
+    });
+}
+
+fn bench_parse_crc_body(c: &mut Criterion) {
+    c.bench_function("bench_parse_crc_body", |b| {
+        let data = include_bytes!("../assets/3381.replay");
+        b.iter(||
+            black_box(ParserBuilder::new(data).always_check_crc().must_parse_network_data().parse().is_ok())
+        );
+    });
+}
+
+fn bench_parse_no_crc_body(c: &mut Criterion) {
+    c.bench_function("bench_parse_no_crc_body", |b| {
+        let data = include_bytes!("../assets/3381.replay");
+        b.iter(||
+            black_box(ParserBuilder::new(data).on_error_check_crc().must_parse_network_data().parse().is_ok())
+        );
+    });
+}
+
+fn bench_parse_no_crc_no_body(c: &mut Criterion) {
+    c.bench_function("bench_parse_no_crc_no_body", |b| {
+        let data = include_bytes!("../assets/3381.replay");
+        b.iter(||
+            black_box(ParserBuilder::new(data).on_error_check_crc().never_parse_network_data().parse().is_ok())
+        );
+    });
+}
+
+fn bench_parse_crc_json(c: &mut Criterion) {
+    c.bench_function("bench_parse_crc_json", |b| {
+        let data = include_bytes!("../assets/3381.replay");
+        b.iter(|| {
+            let data = ParserBuilder::new(data).always_check_crc().parse().unwrap();
+            black_box(serde_json::to_writer(&mut io::sink(), &data).is_ok());
+        });
+    });
+}
+
+fn bench_parse_no_crc_json(c: &mut Criterion) {
+    c.bench_function("bench_parse_no_crc_json", |b| {
+        let data = include_bytes!("../assets/3381.replay");
+        b.iter(|| {
+            let replay = ParserBuilder::new(data).on_error_check_crc().parse().unwrap();
+            black_box(serde_json::to_writer(&mut io::sink(), &replay).is_ok());
+        });
+    });
+}
+
+criterion_group!(benches,
+    bench_crc,
+    bench_parse_crc_body,
+    bench_parse_no_crc_body,
+    bench_parse_no_crc_no_body,
+    bench_parse_crc_json,
+    bench_parse_no_crc_json
+);
+
+criterion_main!(benches);
--- bitter/benches/bitget.rs
+++ bitter/benches/bitget.rs
@@ -1,67 +1,85 @@
-#![feature(test)]
-
+#[macro_use]
+extern crate criterion;
 extern crate bitter;
-extern crate test;
 
-use test::{black_box, Bencher};
+use criterion::{black_box, Criterion};
 use bitter::BitGet;
 
-#[bench]
-fn bench_read_u32_bits_unchecked(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bits = BitGet::new(&[0; 0x10000]);
-        for _ in 0..1000 {
-            black_box(bits.read_u32_bits_unchecked(8));
-        }
+static DATA: [u8; 0x10000] = [0; 0x10000];
+
+fn bench_read_u32_bits_unchecked(c: &mut Criterion) {
+    c.bench_function("read_u32_bits_unchecked", |b| {
+        b.iter(|| {
+            let mut bits = BitGet::new(&DATA);
+            for _ in 0..1000 {
+                black_box(bits.read_u32_bits_unchecked(8));
+            }
+        })
     });
 }
 
-#[bench]
-fn bench_read_u32_bits_checked(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bits = BitGet::new(&[0; 0x10000]);
-        for _ in 0..1000 {
-            black_box(bits.read_u32_bits(8));
-        }
+fn bench_read_u32_bits_checked(c: &mut Criterion) {
+    c.bench_function("read_u32_bits_checked", |b| {
+        b.iter(|| {
+            let mut bits = BitGet::new(&DATA);
+            for _ in 0..1000 {
+                black_box(bits.read_u32_bits(8));
+            }
+        })
     });
 }
 
-#[bench]
-fn bench_read_u8_unchecked(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bits = BitGet::new(&[0; 0x10000]);
-        for _ in 0..1000 {
-            black_box(bits.read_u8_unchecked());
-        }
+fn bench_read_u8_unchecked(c: &mut Criterion) {
+    c.bench_function("read_u8_unchecked", |b| {
+        b.iter(|| {
+            let mut bits = BitGet::new(&DATA);
+            for _ in 0..1000 {
+                black_box(bits.read_u8_unchecked());
+            }
+        })
     });
 }
 
-#[bench]
-fn bench_read_u8_checked(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bits = BitGet::new(&[0; 0x10000]);
-        for _ in 0..1000 {
-            black_box(bits.read_u8());
-        }
+fn bench_read_u8_checked(c: &mut Criterion) {
+    c.bench_function("read_u8_checked", |b| {
+        b.iter(|| {
+            let mut bits = BitGet::new(&DATA);
+            for _ in 0..1000 {
+                black_box(bits.read_u8());
+            }
+        })
     });
 }
 
-#[bench]
-fn bench_read_f32_checked(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bits = BitGet::new(&[0; 0x10000]);
-        for _ in 0..1000 {
-            black_box(bits.read_f32());
-        }
+fn bench_read_f32_checked(c: &mut Criterion) {
+    c.bench_function("read_f32_unchecked", |b| {
+        b.iter(|| {
+            let mut bits = BitGet::new(&DATA);
+            for _ in 0..1000 {
+                black_box(bits.read_f32());
+            }
+        })
     });
 }
 
-#[bench]
-fn bench_read_f32_unchecked(b: &mut Bencher) {
-    b.iter(|| {
-        let mut bits = BitGet::new(&[0; 0x10000]);
-        for _ in 0..1000 {
-            black_box(bits.read_f32_unchecked());
-        }
+fn bench_read_f32_unchecked(c: &mut Criterion) {
+    c.bench_function("read_f32_checked", |b| {
+        b.iter(|| {
+            let mut bits = BitGet::new(&[0; 0x10000]);
+            for _ in 0..1000 {
+                black_box(bits.read_f32_unchecked());
+            }
+        })
     });
 }
+
+criterion_group!(benches,
+    bench_read_u32_bits_unchecked,
+    bench_read_u32_bits_checked,
+    bench_read_u8_unchecked,
+    bench_read_u8_checked,
+    bench_read_f32_unchecked,
+    bench_read_f32_checked
+);
+
+criterion_main!(benches);
