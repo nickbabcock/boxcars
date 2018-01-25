d3f7c91f73f1e79403faeadda1d835d4f4d40ab0
Nick Babcock <nbabcock19@hotmail.com>
2018-01-25 00:26:40 -0500

    CI: build and test all projects

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross build --all --target $TARGET
+    cross build --all --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cross test --all --target $TARGET
+    cross test --all --target $TARGET --release
 
     (cross run -p rrrocket --target $TARGET -- --help)
     (cross run -p rrrocket --target $TARGET --release -- --help)
