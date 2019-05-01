4ef221461f5aba5c066e7c8a8be5c229788e12ed
Nick Babcock <nbabcock19@hotmail.com>
2019-05-01 18:22:57 -0500

    CI cargo cache

--- ci/script.sh
+++ ci/script.sh
@@ -5,14 +5,12 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     cross build --all --target $TARGET
-    cross build --all --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
     cross test --all --target $TARGET
-    cross test --all --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
