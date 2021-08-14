a502037872c0d64d4e5435073cecb0f76a7185a4
Nick Babcock <nbabcock19@hotmail.com>
2021-08-13 23:08:57 -0500

    Add support for RL v2.01

--- src/data.rs
+++ src/data.rs
@@ -169,6 +169,7 @@ pub(crate) static ATTRIBUTES: phf::Map<&'static str, AttributeTag> = phf::phf_ma
     "TAGame.Car_TA:ReplicatedCarScale" => AttributeTag::Float,
     "TAGame.Car_TA:ReplicatedDemolish" => AttributeTag::Demolish,
     "TAGame.Car_TA:ReplicatedDemolish_CustomFX" => AttributeTag::DemolishFx,
+    "TAGame.Car_TA:ReplicatedDemolishGoalExplosion" => AttributeTag::DemolishFx,
     "TAGame.Car_TA:RumblePickups" => AttributeTag::ActiveActor,
     "TAGame.RumblePickups_TA:ConcurrentItemCount" => AttributeTag::Int,
     "TAGame.RumblePickups_TA:AttachedPickup" => AttributeTag::ActiveActor,
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -89,3 +89,4 @@ frame_len_test!(test_rumble_pickup_replay, "../assets/replays/good/140a5.replay"
 frame_len_test!(test_rlcs2_replay, "../assets/replays/good/rlcs2.replay", 10664);
 frame_len_test!(test_00bb_replay, "../assets/replays/good/00bb.replay", 8175);
 frame_len_test!(test_gridiron_replay, "../assets/replays/good/gridiron.replay", 11118);
+frame_len_test!(test_rl_2_replay, "../assets/replays/good/5f97d.replay", 10780);
